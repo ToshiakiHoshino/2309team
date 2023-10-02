9c1677e2a1f615ea0e6a0922ab01ff18efadf6b5
ToshiakiHoshino <134285052+ToshiakiHoshino@users.noreply.github.com>
2023-10-02 20:56:29 +0900

    ExpenseAdjustment　機能実装　03　（星野）コメントアウト処理

--- demo2309/src/main/java/com/example/demo/entity/LeavingEntity.java
+++ demo2309/src/main/java/com/example/demo/entity/LeavingEntity.java
@@ -1,47 +1,47 @@
-package com.example.demo.entity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
-
-//出勤情報
-
-@Data
-@Entity
-@Table(name = "attendance" , schema = "public")
-public class LeavingEntity{
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "userid")
-	private Integer userid;
-	
-	@Column(name = "status")
-	private String status;
-	
-	@Column(name = "end_day")
-	private String end_day;
-	
-	@Column(name = "end_time")
-	private String end_time;
-	
-	@Column(name = "remarks")
-	private String remarks;
-	
-	@Column(name = "Break_time")
-	private String break_time;
-
-	public void setUserid(String userid2) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-	}
-
-	public void setAttendance_id(Object attendance_id) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-	}
-}
+//package com.example.demo.entity;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//import lombok.Data;
+//
+////出勤情報
+//
+//@Data
+//@Entity
+//@Table(name = "attendance" , schema = "public")
+//public class LeavingEntity{
+//	
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@Column(name = "userid")
+//	private Integer userid;
+//	
+//	@Column(name = "status")
+//	private String status;
+//	
+//	@Column(name = "end_day")
+//	private String end_day;
+//	
+//	@Column(name = "end_time")
+//	private String end_time;
+//	
+//	@Column(name = "remarks")
+//	private String remarks;
+//	
+//	@Column(name = "Break_time")
+//	private String break_time;
+//
+//	public void setUserid(String userid2) {
+//		// TODO 自動生成されたメソッド・スタブ
+//		
+//	}
+//
+//	public void setAttendance_id(Object attendance_id) {
+//		// TODO 自動生成されたメソッド・スタブ
+//		
+//	}
+//}
