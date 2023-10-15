144a444c4a2c94b895a54364a64b4eea9721c3a5
sugimai813 <maaaimaaai813@gmail.com>
2023-10-15 19:17:01 +0900

    勤怠一覧1015

---
+++ demo2309/src/main/java/com/example/demo/controller/AttendanceListController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.AttendanceListEntity;
+import com.example.demo.service.AttendanceListService;
+
+@Controller
+public class AttendanceListController {
+	@Autowired
+    AttendanceListService attendanceListService;
+	
+//勤怠一覧を表示
+	@GetMapping("/user/attendance_list")
+	public String displayList(Model model) {
+		List<AttendanceListEntity> userlist = attendanceListService.searchAll();
+		model.addAttribute("userlist", userlist);
+		return "attendance_list";
+				
+	}
+}
